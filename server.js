46c65e9c393a745f152c3da317adb1e19c71c59e
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2022-04-14 21:38:30 +0200

    asdf

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+//const PORT = 3000;
 
 app.get("/", (req, res)=>{
     res.sendFile(`${__dirname}/index.html`);
 })
 
-app.listen(PORT, ()=>{});
+app.listen(3000);
